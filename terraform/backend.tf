35024924cdf501429784fbd78a68a5788327ad92
cumulusaws22 <cumulus.aws22@gmail.com>
2026-08-11 08:25:11 +0530

    Update Terraform backend configuration: unify state file key and remove unused backend block

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,8 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-state-200845569305-us-east-1"
-    key     = "dev/terraform.tfstate"
+    key     = "backend/terraform-state.tfstate"
     region  = "us-east-1"
-    encrypt = true
-  }
-
-    backend "s3" {
-    bucket  = "terraform-state-200845569305-us-east-2"
-    key     = "uat/terraform.tfstate"
-    region  = "us-east-2"
     encrypt = true
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region for the Organizations API"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "scp_name_prefix" {
