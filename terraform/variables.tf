e9e935a576208d3ffe738cf5b6274df01b2d51f0
cumulusaws22 <cumulus.aws22@gmail.com>
2026-08-11 21:11:21 +0530

    Remove target_accounts variable from variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,10 +30,3 @@ variable "identity_center_group_id" {
   type        = string
 }
 
-variable "target_accounts" {
-  description = "AWS accounts to which the CloudOps permission set should be assigned"
-  type = map(object({
-    account_id = string
-    account_name = string
-  }))
-}
