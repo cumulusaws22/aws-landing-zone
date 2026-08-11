31149ffb0ae55e5d4c4aab2bae68c737ab442f42
cumulusaws22 <cumulus.aws22@gmail.com>
2026-08-11 09:04:16 +0530

    Add Terraform outputs for restrict regions and deny public S3 policies

---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "restrict_regions_policy_id" {
+  value = aws_organizations_policy.restrict_regions.id
+}
+
+output "deny_public_s3_policy_id" {
+  value = aws_organizations_policy.deny_public_s3.id
+}
