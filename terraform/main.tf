b505ae093daef1d72437812a8366f1e057817b1f
cumulusaws22 <cumulus.aws22@gmail.com>
2026-08-11 12:53:39 +0530

    Refactor region restriction policies and attachments for clarity

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+
+################################################################
+# Region Restriction SCPs
+################################################################
+
 resource "aws_organizations_policy" "restrict_regions" {
   name        = "${var.scp_name_prefix}-Restrict-Regions"
   description = "Restrict AWS resource creation outside approved regions"
@@ -9,6 +14,15 @@ resource "aws_organizations_policy" "restrict_regions" {
     Purpose   = "RegionRestriction"
   }
 }
+
+resource "aws_organizations_policy_attachment" "restrict_regions_prod" {
+  policy_id = aws_organizations_policy.restrict_regions.id
+  target_id = var.non-prod_ou_id
+}
+
+##################################################################
+# Restrict Public Access To S3
+##################################################################
 
 resource "aws_organizations_policy" "deny_public_s3" {
   name        = "${var.scp_name_prefix}-Deny-Public-S3"
@@ -22,12 +36,25 @@ resource "aws_organizations_policy" "deny_public_s3" {
   }
 }
 
-resource "aws_organizations_policy_attachment" "restrict_regions_prod" {
-  policy_id = aws_organizations_policy.restrict_regions.id
-  target_id = var.non-prod_ou_id
-}
-
 resource "aws_organizations_policy_attachment" "deny_public_s3_prod" {
   policy_id = aws_organizations_policy.deny_public_s3.id
   target_id = var.non-prod_ou_id
+}
+
+##################################################
+# Region Restriction Guardrail
+##################################################
+
+resource "aws_controltower_control" "region_deny" {
+
+  for_each = var.region_deny_ous
+
+  control_identifier = "arn:aws:controltower:${var.control_tower_home_region}::control/CT.MULTISERVICE.PV.1"
+
+  target_identifier = each.value
+
+  parameters {
+    key   = "AllowedRegions"
+    value = join(",", var.allowed_regions)
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,25 @@ variable "non-prod_ou_id" {
   type        = string
   default     = "ou-slmb-ytdz020q"
 }
+
+
+
+variable "region_deny_ous" {
+  description = "OU ARNs where Region Deny should be enabled"
+  type        = map(string)
+
+  default = {
+    #uat        = "arn:aws:organizations::123456789012:ou/o-example/ou-prod"
+    nonprod    = "arn:aws:organizations::200845569305:ou/o-yzrg6g6dd8/ou-slmb-ytdz020q"
+  }
+}
+
+variable "allowed_regions" {
+  description = "AWS Regions allowed for workloads"
+  type        = list(string)
+
+  default = [
+    "us-east-1",
+    "us-east-2"
+  ]
+}
